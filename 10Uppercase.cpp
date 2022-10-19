35b0a3b6a6c14b4a7d73020178fb89c57f0c0b74
prasadpisal4cctech <112372385+prasadpisal4cctech@users.noreply.github.com>
2022-10-19 11:58:25 +0530

    Add files via upload

--- 10Uppercase.cpp
+++ 10Uppercase.cpp
@@ -1,19 +1,20 @@
-//Write a program to accept a string and convert all its characters to upper case.
-#include<iostream>
+// Write a program to accept a string and convert all its characters to upper case.
+#include <iostream>
+#include <string>
+#include <cctype>
+
 using namespace std;
 int main()
 {
-     
     string str;
-    
     cout << "Enter a string" << endl;
     getline(cin, str);
-for (int i = 0; i < str.length(); i++)
-{
-    str[i]=str[i]-32;
-}
-   cout<<str;
-   
-
-return 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if(islower(str[i]))
+        {
+           str[i] = str[i] - 32;
+        }
+    }
+    cout << str;
 }
--- 11lowerCase.cpp
+++ 11lowerCase.cpp
@@ -1,19 +1,23 @@
-//Write a program to accept a string and convert all its characters to lower case.
-#include<iostream>
+// Write a program to accept a string and convert all its characters to lower case.
+#include <iostream>
+#include <string>
+
 using namespace std;
 int main()
 {
-     
+
     string str;
-    
+
     cout << "Enter a string" << endl;
     getline(cin, str);
-for (int i = 0; i < str.length(); i++)
-{
-    str[i]=str[i]+32;
-}
-   cout<<str;
-   
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + 32;
+        };
+    }
+    cout << str;
 
-return 0;
+    return 0;
 }
